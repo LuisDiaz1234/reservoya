a1e3aa869dba6387de2f154b37ba6d6c186526df
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-19 16:28:27 -0600

    Create next-env.d.ts

---
+++ apps/web/next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: Este archivo lo suele generar Next automáticamente.
+// Lo incluimos para evitar advertencias en el build.
