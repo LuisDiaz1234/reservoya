98481b68af8fd9804d6749326a807ce986bab8da
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-20 17:49:09 -0600

    Update next-env.d.ts

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,5 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-
-// NOTE: Este archivo lo suele generar Next automáticamente.
-// Lo incluimos para evitar advertencias en el build.
+// NO EDITAR: requerido por Next para compilar TypeScript
