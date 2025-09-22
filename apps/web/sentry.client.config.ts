9f2111f895e1907fd48081d7d8e9243c35ea0270
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-21 23:22:45 -0600

    Create sentry.client.config.ts

---
+++ apps/web/sentry.client.config.ts
@@ -0,0 +1,5 @@
+// apps/web/sentry.client.config.ts
+import * as Sentry from '@sentry/nextjs';
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.NEXT_PUBLIC_SENTRY_DSN });
+}
